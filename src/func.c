f81f7e020b52c584ff9d1c4d5cd07268ce824e90
Sávio Matheus <saviomatheus@discente.ufg.br>
2020-10-17 17:38:30 -0300

    Nova pasta: src

---
+++ src/func.c
@@ -0,0 +1,11 @@
+/*
+	Funções que implementam as funcionalidades principais do
+	programa e são chamadas na main(), e outras que não se
+	encaixam nos outros módulos.
+*/
+
+#include <stdio.h>
+
+#include "arquivo.h"
+#include "tad.h"
+#include "func.h"
---
+++ src/tad.c
@@ -0,0 +1,13 @@
+/*
+	Estruturas de dados e funções que trabalham sobre elas.
+*/
+
+#include <stdio.h>
+
+#include "tad.h"
+
+// Vetor
+
+// Fila
+
+// Lista duplamente encadeada
