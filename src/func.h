279912db60f56a16f5ad7a59782d43d22915f1f9
Sávio Matheus <saviomatheus@discente.ufg.br>
2020-10-31 20:09:25 -0300

    Protótipos de funções do CRUD Pacientes

--- src/func.h
+++ src/func.h
@@ -3,6 +3,21 @@
 
 // Typedefs
 
-// Funções
+// Funções expostas para o main.c. Elas devem chamar outras funções dentro de func.c
+void cadastrarPaciente();
+void editarPaciente();
+void listarPacientes();
+void removerPaciente();
+
+/*
+Funções do CRUD consultas. Não implementar por enquanto.
+void cadastrar_consulta();
+void editar_consulta();
+void listar_consultas();
+void remover_consulta();
+*/
+
+void carregarArquivo();
+void salvarArquivo();
 
 #endif /* FUNCIONALIDADE_H */
