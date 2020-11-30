a9d5d028d7d5494eda4129524ad3989ed2957997
Sávio Matheus <saviomatheus@discente.ufg.br>
2020-11-30 10:46:55 -0300

    Atualiza o script

--- compilar.bat
+++ compilar.bat
@@ -1,9 +1,5 @@
-@echo on
-
 gcc -Wall -pedantic -c src\tad.c
-gcc -Wall -pedantic -c src\arquivo.c
-gcc -Wall -pedantic -c src\func.c
 gcc -Wall -pedantic -c src\main.c
-gcc -Wall -pedantic -o Projeto_AED_I.exe main.o tad.o arquivo.o func.o
+gcc -Wall -pedantic -o Projeto_AED_I.exe main.o tad.o
 
 del *.o
--- compilar.sh
+++ compilar.sh
@@ -1,10 +1,8 @@
 #! /bin/bash
 
 gcc -Wall -pedantic -c src/tad.c
-gcc -Wall -pedantic -c src/arquivo.c
-gcc -Wall -pedantic -c src/func.c
 gcc -Wall -pedantic -c src/main.c
-gcc -Wall -pedantic -o Projeto_AED_I.out main.o tad.o arquivo.o func.o
+gcc -Wall -pedantic -o Projeto_AED_I.out main.o tad.o
 
 rm *.o
 
