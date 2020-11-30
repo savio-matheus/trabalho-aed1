8201b14b708c03b2622b84f8bc77547e9006293e
Sávio Matheus <saviomatheus@discente.ufg.br>
2020-11-30 10:41:12 -0300

    Adiciona as funções do arquivo func.c

--- src/main.c
+++ src/main.c
@@ -8,9 +8,92 @@ Alunos: Cláudio da Silva Pinheiro Júnior
 */
 
 #include <stdio.h>
+#include <stdlib.h>
 
-#include "func.h"
-#include "lista.h"
+#include "tad.h"
+
+int validaCpf (char cpf[], int dim){
+    int tamanho = dim;
+
+    //Validando tamanho do vetor enviado (11 caracteres caso sejam armazenados apenas numeros ou 14 caso seja armazenada a pontuacao)
+    if(tamanho == 11){
+        int i;
+        //Validando se apenas numeros foram digitados
+        for(i = 0; i<=tamanho; i++){
+            if(!isdigit(cpf[i])){
+                return 1;
+            }
+        }
+    }else{
+        return 1;
+    }
+    //sucesso
+    return 0;
+}
+
+// cadastrarPaciente cria um Paciente e o retorna
+PACIENTE* cadastrarPaciente(){
+
+    PACIENTE *paciente = (PACIENTE*)malloc(sizeof(PACIENTE));
+    char *tmp = (char*)malloc(sizeof(char)*30);
+
+	printf("\nInforme o nome do paciente: ");
+    fflush(stdin);
+    fgets(paciente->nome, sizeof(paciente->nome), stdin);
+
+    printf("Informe a data de nascimento: ");
+    fflush(stdin);
+	fgets(paciente->dataDeNascimento, sizeof(paciente->dataDeNascimento), stdin);
+
+	printf("Informe o sexo do paciente (M/F): ");
+    fflush(stdin);
+	fgets(paciente->sexo, sizeof(paciente->sexo), stdin);
+
+	printf("Informe o CPF do paciente: ");
+    fflush(stdin);
+	fgets(paciente->CPF, sizeof(paciente->CPF), stdin);
+
+	printf("Informe o peso do paciente: ");
+    fflush(stdin);
+    fgets(tmp, sizeof(tmp), stdin);
+    paciente->peso = strtof(tmp, NULL);
+
+	printf("Informe a altura do paciente: ");
+    fflush(stdin);
+    fgets(tmp, sizeof(char *), stdin);
+    paciente->altura = strtof(tmp, NULL);
+
+    free(tmp);
+	return paciente;
+
+}
+
+// toString recebe uma struct Paciente e a imprime 
+void toString(PACIENTE *paciente){
+
+    printf("\nNome do paciente: %s", paciente->nome);
+    printf("Data de nascimento: %s", paciente->dataDeNascimento);
+    printf("Sexo do paciente (M/F): %s", paciente->sexo);
+    printf("CPF do paciente: %s", paciente->CPF);
+    printf("Peso do paciente: %.2f", paciente->peso);
+    printf("\nAltura do paciente: %.2f", paciente->altura);
+
+}
+
+void editarPaciente()
+{}
+
+void listarPacientes()
+{}
+
+void removerPaciente()
+{}
+
+void carregarArquivo()
+{}
+
+void salvarArquivo()
+{}
 
 int painel(){
     int opcao;
